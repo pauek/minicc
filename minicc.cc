9fb0eb0eb633be5225f04b7d42f2a9f7aba40ffa
Pau Fernández <pau.fernandez@upc.edu>
2014-07-12 11:56:38 +0200

    MiniC++

---
+++ minicc.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Pos { 
+   int lin, col; 
+
+   Pos() : lin(1), col(0) {}
+};
+
+ostream& operator<<(ostream& o, const Pos& pos) {
+   o << pos.lin << ":" << pos.col;
+   return o;
+}
+
+class Input {
+   istream* _in;
+   string _text;
+   int _curr;
+   Pos _pos;
+
+   void _reset() { 
+      _pos.lin = 1;
+      _pos.col = -1;
+      _curr = -1; 
+   }
+
+public:
+   Input() : _in(0) { 
+      _reset(); 
+   }
+
+   Input(istream& i) {
+      _reset();
+      _in = &i;
+   }
+
+   Input(string filename) {
+      _reset();
+      _in = new ifstream(filename.c_str());
+   }
+
+   ~Input() {
+      if (istream* f = dynamic_cast<ifstream*>(_in)) {
+         delete f;
+      }
+   }
+
+   Pos  pos() const { return _pos; }
+   bool next();
+   char curr() const { return _text[_curr]; }
+};
+
+bool Input::next() {
+   if (_in == 0) {
+      return false;
+   }
+   _curr++;
+   _pos.col++;
+   while (1) {
+      if (_curr >= _text.size()) {
+         if (!_in->good()) {
+            return false;
+         }
+         string line;
+         getline(*_in, line);
+         _text += line + "\n";
+      }
+      if (_text[_curr] == '\n') {
+         _pos.lin++;
+         _pos.col = 0;
+         _curr++;
+      } else {
+         break;
+      }
+   }
+   return true;
+}
+
+int main() {
+   Input I(cin);
+   char c;
+   while (I.next()) {
+      cout << "'" << I.curr() << "' " << I.pos() << endl;
+   }
+}
