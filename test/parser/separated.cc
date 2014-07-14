ff092c3816df471b6deb72452177137172888b65
Pau Fernández <pau.fernandez@upc.edu>
2014-07-14 22:30:20 +0200

    Añado más tests

---
+++ test/parser/separated.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+int main() 
+{}
+
+
+[[out]]--------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {}
+[[err]]--------------------------------------------------
