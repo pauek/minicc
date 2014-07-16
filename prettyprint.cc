c2c04398a01b5e39f1ec0333603307162fb56aac
Pau Fernández <pau.fernandez@upc.edu>
2014-07-16 11:28:36 +0200

    Refactor de prettyprint::_cmt*

--- prettyprint.cc
+++ prettyprint.cc
@@ -12,10 +12,10 @@ std::ostream& PrettyPrinter::out(OutType typ) {
    return *_out; 
 }
 
-string _cmt(string sp, CommentNode* cn) {
+string _cmt(CommentNode* cn, int n = 1) {
    ostringstream out;
    if (cn == 0) {
-      out << sp;
+      out << string(n, ' ');
    } else {
       out << ' ' << cn << ' ';
    }
@@ -33,6 +33,19 @@ string _cmt_endl(CommentNode *cn) {
    return out.str();
 }
 
+string _cmt(AstNode* x, int i, int n = 1) {
+   return _cmt(x->comment_nodes[i], n);
+}
+
+string _cmt0(AstNode* x, int i) {
+   return _cmt(x->comment_nodes[i], 0);
+}
+
+string _cmt_endl(AstNode* x, int i) {
+   return _cmt_endl(x->comment_nodes[i]);
+}
+
+
 void PrettyPrinter::visit_comment(CommentNode* cn) {
    out() << cn;
 }
@@ -40,10 +53,10 @@ void PrettyPrinter::visit_comment(CommentNode* cn) {
 void PrettyPrinter::visit_include(Include* x) {
    string delim = "\"\"";
    if (x->global) delim = "<>";
-   out() << "#" << _cmt("", x->comment_nodes[0])
-         << "include" << _cmt(" ", x->comment_nodes[1])
+   out() << "#" << _cmt0(x, 0)
+         << "include" << _cmt(x, 1)
          << delim[0] << x->filename << delim[1]
-         << _cmt_endl(x->comment_nodes[2]);
+         << _cmt_endl(x, 2);
 }
 
 void PrettyPrinter::visit_macro(Macro* x) {
@@ -51,10 +64,10 @@ void PrettyPrinter::visit_macro(Macro* x) {
 }
 
 void PrettyPrinter::visit_using(Using* x) {
-   out() << "using" << _cmt(" ", x->comment_nodes[0]) 
-         << "namespace" << _cmt(" ", x->comment_nodes[1])
-         << x->namespc << _cmt("", x->comment_nodes[2])
-         << ";" << _cmt_endl(x->comment_nodes[3]);
+   out() << "using" << _cmt(x, 0)
+         << "namespace" << _cmt(x, 1)
+         << x->namespc << _cmt0(x, 2)
+         << ";" << _cmt_endl(x, 3);
 }
 
 void PrettyPrinter::visit_nodelist(NodeList* x) {}
@@ -66,17 +79,17 @@ void PrettyPrinter::visit_type(Type *x) {
 void PrettyPrinter::visit_funcdecl(FuncDecl *x) {
    out() << endl;
    visit_type(x->return_type);
-   out() << _cmt(" ", x->comment_nodes[0]) 
-         << x->name << _cmt("", x->comment_nodes[1]) << "(";
+   out() << _cmt(x, 0)
+         << x->name << _cmt0(x, 1) << "(";
    for (int i = 0; i < x->params.size(); i++) {
       if (i > 0) {
          out() << ",";
       }
-      out() << _cmt( (i > 0 ? " " : ""), x->params[i].c[0]);
+      out() << _cmt(x->params[i].c[0], (i > 0 ? 1 : 0));
       visit_type(x->params[i].type);
-      out() << _cmt(" ", x->params[i].c[1]);
+      out() << _cmt(x->params[i].c[1]);
       out() << x->params[i].name;
-      out() << _cmt("", x->params[i].c[2]);
+      out() << _cmt(x->params[i].c[2], 0);
    }
    out() << ") ";
    visit_block(x->block);
@@ -97,5 +110,5 @@ void PrettyPrinter::visit_block(Block *x) {
 }
 
 void PrettyPrinter::visit_stmt(Statement *x) {
-   out(beginl) << ";" << _cmt_endl(x->comment_nodes[0]);
+   out(beginl) << ";" << _cmt_endl(x, 0);
 }
