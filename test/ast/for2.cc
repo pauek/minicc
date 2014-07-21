883230f63627691670155c9fd722860da3a083e7
Pau Fernández <pau.fernandez@upc.edu>
2014-07-21 21:14:21 +0200

    Más tests del for, con DeclStmt + Expr(,)

---
+++ test/ast/for2.cc
@@ -0,0 +1,18 @@
+void print() {
+   for (int i = 1; i < 100; i = i + 1) {
+      cout << i << endl;
+   }
+}
+[[out]]--------------------------------------------------
+Program{
+   FuncDecl("print", Type(void), Params = {}, {
+      Block({
+         IterStmt<for>(DeclStmt(Type(int), Vars = {"i" = lit:'1'}), <(id:'i', lit:'100'), =(id:'i', +(id:'i', lit:'1')), {
+            Block({
+               Stmt(expr, <<(<<(id:'cout', id:'i'), id:'endl'))
+            })
+         })
+      })
+   })
+}
+[[err]]--------------------------------------------------
---
+++ test/ast/for3.cc
@@ -0,0 +1,18 @@
+void print() {
+   for (int i = 1, j = i; i < 100 && j < 100; i = i + 1, j = j + 1) {
+      cout << i << endl;
+   }
+}
+[[out]]--------------------------------------------------
+Program{
+   FuncDecl("print", Type(void), Params = {}, {
+      Block({
+         IterStmt<for>(DeclStmt(Type(int), Vars = {"i" = lit:'1', "j" = id:'i'}), &&(<(id:'i', lit:'100'), <(id:'j', lit:'100')), ,(=(id:'i', +(id:'i', lit:'1')), =(id:'j', +(id:'j', lit:'1'))), {
+            Block({
+               Stmt(expr, <<(<<(id:'cout', id:'i'), id:'endl'))
+            })
+         })
+      })
+   })
+}
+[[err]]--------------------------------------------------
