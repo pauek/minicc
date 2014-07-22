30a9f16b6025a2c90c77cb28447b6f8018b5180b
Pau Fernández <pau.fernandez@upc.edu>
2014-07-22 15:27:10 +0200

    Paso los tests a 60 columnas

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+colsize=60
+
 function test_dir() {
    dir=$(echo $1 | tr -d './');
    printf "%10s  " $dir
-   for ccfile in $(find $dir -name "*.cc" | sort); do
-      ../minicc --test-${dir} $ccfile 2>> ${dir}-err
-      code=$?
-      if [ $code -ne 0 ]; then
-         echo "[error code $code in $ccfile]" >> ${dir}-err
-         echo -n "E"
+   for ccfile in $(find $dir -name "*.cc" | sort | xargs -n $colsize | sed 's/$/ <endl>/'); do
+      if [ $ccfile = "<endl>" ]; then
+         printf "\n%10s  " ""
+      else
+         ../minicc --test-${dir} $ccfile 2>> ${dir}-err
+         code=$?
+         if [ $code -ne 0 ]; then
+            echo "[error code $code in $ccfile]" >> ${dir}-err
+            echo -n "E"
+         fi
       fi
    done
    echo
