784ccb965e1798f47f5bd8a01c11c7527ccfe999
Pau Fernández <pau.fernandez@upc.edu>
2014-07-23 22:10:15 +0200

    Test ilógico fuera

--- test/print/include_inconsistent_delimiters.cc
+++
@@ -1,8 +0,0 @@
-#include "asdf.h>
-#include <293874"
-[[out]]---------------------------------------------
-#include "asdf.h"
-#include <293874>
-[[err]]---------------------------------------------
-1:16: '#include' inconsistent delimiter '>'
-2:16: '#include' inconsistent delimiter '"'
