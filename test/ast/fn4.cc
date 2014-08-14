e88d46ba2c4f83b7abc212f9854633d3379d0bb1
Pau Fernández <pau.fernandez@upc.edu>
2014-08-14 16:17:25 +0200

    Otro test

---
+++ test/ast/fn4.cc
@@ -0,0 +1,6 @@
+vector<int> f(int x);
+[[out]]----------------------------------------
+Program{
+   FuncDecl("f", Type(id:'vector'<Type(id:'int')>), Params = {"x": Type(id:'int')})
+}
+[[err]]----------------------------------------
