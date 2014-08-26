ab532e526709306c64121b5a11378639cab1a4a2
Pau Fernández <pau.fernandez@upc.edu>
2014-08-26 15:03:25 +0200

    Amplio un poco el test de literales negativos

--- test/ast/negint.cc
+++ test/ast/negint.cc
@@ -1,11 +1,13 @@
 void f() {
    a = -1;
+   b = -0.5;
 }
 [[out]]--------------------------------------------------
 Program{
    FuncDecl("f", Type(id:'void'), Params = {}, {
       Block({
          ExprStmt(=(id:'a', Int<-1>))
+         ExprStmt(=(id:'b', Double<-0.5>))
       })
    })
 }
