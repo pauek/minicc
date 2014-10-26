59074062575e9423a31179ddd451e8ec14c295ab
Pau Fernández <pau.fernandez@upc.edu>
2014-10-26 21:15:13 +0100

    Added some tests (that pass)

---
+++ test/print/expr_parentheses1_comment3.cc
@@ -0,0 +1,7 @@
+double    fn   (char c1 ,char   c2  )    {
+   a=(/* yay! */a+ 1 );
+}
+[[out]]--------------------------------------------------
+double fn(char c1, char c2) {
+   a = (/* yay! */ a + 1);
+}
