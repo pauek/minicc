9f314e4509314c4f626be4ca853fd72be97d82b6
Pau Fernández <pau.fernandez@upc.edu>
2014-11-22 22:43:38 +0100

    Reimplemento el pintado del entorno usando KineticJS
    - Aunque el problema del zoom en la página es inaceptable, cambiaré a RaphaelJS.

--- web/js/main.js
+++ web/js/main.js
@@ -31,7 +31,8 @@ function setCompilado(new_value) {
       stepper.clearMark();
    }
    slider.reset();
-   showstate(null);
+   // showstate(null);
+   draw_state(null);
 }
 
 var editor;
@@ -164,7 +165,8 @@ var stepper = {
       }
       var item = stepper._history[k];
       this.setMark(item.span);
-      showstate(item);
+      // showstate(item);
+      draw_state(item);
    }
 };
 
@@ -247,8 +249,279 @@ function value_str(value, addClass, insert) {
    };
 }
 
+/* Draw functions
+ 
+   These functions draw the state of the environment to a canvas
+   using KineticJS. 
+
+   All draw functions return a single object (a group if necessary), and
+   the size and position of this object.
+
+       drawX(x, y, data) -> {obj, width, height}
+
+*/
+
+var draw_params = {
+   lineHeight: 24,
+   var_name_sep: 6,
+   value_sep: 7,
+   value_padding: 4,
+   value_padding_left: 7,
+   obj_padding: 1,
+   env_padding: 8,
+   env_padding_left: 12,
+   min_env_width: 100,
+   min_env_height: 10,
+};
+
+var stage = null;
+
+function draw_text(x, y, text, fill, fontSize) {
+   var text = new Kinetic.Text({
+      x: x, y: y,
+      text: text,
+      fontSize: (fontSize ? fontSize : 14),
+      fontFamily: 'Source Code Pro',
+      fill: (fill ? fill : '#333'),
+   });
+   return {
+      obj:    text,
+      width:  text.width(),
+      height: text.height()
+   };
+}
+
+function draw_state(S) {
+   stage.destroyChildren();
+   $('#status').text('');
+   if (S === null) {
+      $('#slider .knob').addClass('disabled');
+      return;
+   }
+   $('#slider .knob').removeClass('disabled');
+   var layer = new Kinetic.Layer();
+   var x = 10, y = 10;
+   for (var i = S.env.length-1; i >= 0; i--) {
+      var env = draw_env(x, y, S.env[i]);
+      layer.add(env.obj);
+      x += env.width + 20;
+   }
+   stage.add(layer);
+}
+
+function draw_env(_x, _y, _env) {
+   var frame = new Kinetic.Group({x: _x, y: _y, id: _env.name});
+   var x = 0, y = 0;
+
+   // title
+   var title = draw_text(x+10, y+9, _env.name);
+   frame.add(title.obj);
+   y += draw_params.lineHeight + 10;
+   var title_height = y;
+
+   // vars + values
+   var P  = draw_params.env_padding;
+   var PL = draw_params.env_padding_left;
+   var env = draw_obj(x + PL, y + P, _env.tab);
+   frame.add(env.obj);
+   y += env.height;
+
+   var W = draw_params.min_env_width;
+   var H = draw_params.min_env_height;
+   var width  = (env.width  < W ? W : env.width) + 2*P;
+   var height = (env.height < H ? H : env.height) + title_height;
+   if (env.count > 0) {
+      height += 2*P;
+   }
+
+   // draw background + title
+   var active = _env.tab['<active>'];
+   var r1 = new Kinetic.Rect({
+      x: 0, y: 0,
+      width: width, height: height,
+      fill: (active ? 'rgb(255, 255, 160)' : '#f7f7f7'),
+   });
+   var r2 = new Kinetic.Rect({
+      x: 0, y: 0,
+      width: width, height: title_height,
+      fill: (active ? 'rgba(150, 150, 0, 0.3)' : 'rgba(0,0,0,0.2)'),
+   });
+   var r3 = new Kinetic.Rect({
+      x: 0, y: 0,
+      width: width, height: height,
+      stroke: (active ? 'rgba(150, 150, 0, 0.8)' : 'rgba(0,0,0,0.3)'),
+      strokeWidth: 1
+   })
+   frame.add(r1); frame.add(r2); frame.add(r3);
+   r3.moveToBottom(); r2.moveToBottom();  r1.moveToBottom();
+
+   return {
+      obj:    frame,
+      width:  width,
+      height: height,
+      count:  env.count,
+   };
+}
+
+function draw_value(x, y, data, index) {
+   var x0 = x, y0 = y;
+   var group = new Kinetic.Group({x: x, y: y});
+   var P  = draw_params.value_padding;
+   var PL = draw_params.value_padding;
+   var fill = 'rgba(255, 255, 255, 0.6)';
+   if (data instanceof Object) {
+      PL = draw_params.value_padding_left;
+   }
+   var y0 = y, value = {};
+   if (data instanceof Array) {
+      value = draw_array(0, 0, data);
+      y += value.height;
+   } else if (data instanceof Object && data['<type>'] === 'char') {
+      value = draw_text(PL, P, "'" + data['char'] + "'");
+      y += draw_params.lineHeight;
+   } else if (data instanceof Object && data['<type>'] === 'ref') {
+      value = draw_text(PL, P, data['ref']);
+      y += draw_params.lineHeight;
+   } else if (data instanceof Object) {
+      value = draw_obj(PL, P, data);
+      y += value.height;
+   } else {
+      value = draw_text(PL, P, data, '#55f');
+      y += draw_params.lineHeight;
+      fill = 'white';
+   }
+   var height = value.height;
+   var width = value.width;
+   if (!(data instanceof Array)) {
+      var rect = new Kinetic.Rect({
+         x: 0, 
+         y: 0,
+         width:  value.width + P + PL,
+         height: value.height + 2*P,
+         fill:   fill,
+         stroke: '#777',
+         strokeWidth: 0.5
+      });
+      group.add(rect);
+      height = rect.height();
+      width = rect.width();
+   }
+   if (index !== undefined) {
+      var idx = draw_text(2.0, 1.5, "" + index, '#bbb', 9);
+      group.add(idx.obj);
+   }
+   group.add(value.obj);
+   return {
+      obj:    group,
+      width:  width,
+      height: height
+   };
+}
+
+function draw_array(x, y, array) {
+   var group = new Kinetic.Group({x: x, y: y});
+   var height = 0;
+   for (var i = 0; i < array.length; i++) {
+      var value = draw_value(x, y, array[i].data, i);
+      x += value.width;
+      group.add(value.obj);
+      if (value.height > height) {
+         height = value.height;
+      }
+   }
+   return {
+      obj:    group,
+      width:  x,
+      height: height
+   }
+}
+
+function draw_obj(_x, _y, obj) {
+   var group = new Kinetic.Group({x: _x, y: _y});
+   var P = draw_params.obj_padding;
+   var x = P, y = P;
+
+   // draw variable names
+   var objs = {names: [], values: [], count: 0};
+   var names_width = 0, values_width = 0;
+   for (var prop in obj) {
+      if (prop == '<type>' || prop == '<active>') {
+         continue;
+      }
+      objs.count++;
+      if (objs.count > 1) {
+         y += draw_params.value_sep;
+      }
+
+      // value
+      var value = draw_value(x, y, obj[prop].data);
+      if (value.width > values_width) {
+         values_width = value.width;
+      }
+      group.add(value.obj);
+      objs.values.push(value.obj);
+
+      // name
+      var name = draw_text(x, y + value.height/2 - 7 /* tamaño fuente? */, prop);
+      if (name.width > names_width) {
+         names_width = name.width;
+      }
+      group.add(name.obj);
+      objs.names.push(name.obj);
+
+      y += value.height;
+   }
+   
+   // adjust names so that they are aligned to the right
+   for (var i = 0; i < objs.count; i++) {
+      objs.names[i].width(names_width);
+      objs.names[i].align('right');
+      var vx = objs.values[i].x();
+      objs.values[i].x(vx + names_width + draw_params.var_name_sep);
+   }
+
+   return {
+      obj:    group,
+      width:  names_width + draw_params.var_name_sep + values_width + 2*P,
+      height: y + 2*P,
+      count:  objs.count,
+   };
+}
+
+// function draw_obj(_x, _y, obj) {
+//    var objgroup = new Kinetic.Group({
+//       x: x, y: y,
+//    });
+//    var x = _x + 3, y = _y + 3;
+//    var max_x = x;
+//    for (var prop in obj) {
+//       if (prop == '<type>') {
+//          continue;
+//       }
+//       var res = drawvar(x, y, prop, obj[prop].data, objgroup);
+//       if (res.xmax > max_x) {
+//          max_x = res.xmax;
+//       }
+//       y += draw_params.lineHeight;
+//    }
+//    var rect = new Kinetic.Rect({
+//       x: _x, y: _y,
+//       width: max_x + 3,
+//       height: y + 6,
+//       fill: 'white',
+//       stroke: '#777',
+//       strokeWidth: 0.5
+//    });
+//    objgroup.add(rect);
+//    rect.moveToBottom();
+//    return {
+//       objgroup: objgroup,
+//       xmax: max_x,
+//       y:    y
+//    };
+// }
+
 function showstate(S) {
-   jsPlumb.detachEveryConnection();
    var links = [];
    $('#env').empty();
    $('#status').text('');
@@ -264,11 +537,11 @@ function showstate(S) {
    var html = '<table><tr>';
    for (var i = env.length-1; i >= 0; i--) {
       html += '<td><div class="fenv';
-      if (env[i].tab["<active>"]) {
+      if (env.tab["<active>"]) {
          html += " active";
       }
-      html += '"><h5>' + env[i].name + '</h5>';
-      var T = env[i].tab;
+      html += '"><h5>' + env.name + '</h5>';
+      var T = env.tab;
       html += '<div class="wrapper"><table>'
       for (var prop in T) {
          if (prop == "<active>") {
@@ -286,12 +559,6 @@ function showstate(S) {
    html += '</tr></table>';
    $('#env').append(html);
    $('#status').text(S.status);
-   for (var i = 0; i < links.length; i++) {
-      jsPlumb.connect({
-         source: links[i].from,
-         target: links[i].to
-      });
-   }
 }
 
 function sliderChange() {
@@ -468,14 +735,12 @@ $(document).ready(function () {
       }
    });
    slider.init();
-   jsPlumb.setContainer('env');
-   jsPlumb.importDefaults({
-      Anchors: ['Center', 'Right'],
-      Endpoints: ['Blank', 'Blank'],
-      EndpointStyle: { fillStyle: '#f00' },
-      Connector: ['Straight', {}],
-      PaintStyle: { lineWidth: 2, strokeStyle: '#f00' },
-      Overlays: [['Arrow', {location: 1, foldback: 0.8, width: 10, length: 7}]],
+   
+   stage = new Kinetic.Stage({
+      container: 'env',
+      width: 1000,
+      height: 1000
    });
-   showstate(null);
+   // showstate(null);
+   draw_state(null);
 });
