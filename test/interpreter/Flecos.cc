df6f5b77cd8d83b2bb78e5a89a4a4b3777de43c9
Pau Fernández <pau.fernandez@upc.edu>
2014-12-09 17:32:17 +0100

    Otro test de 'getline'

---
+++ test/interpreter/Flecos.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string L, s;
+   getline(cin, L);
+   while (L != ".") {
+      s += L[L.size()-1];
+      getline(cin, L);
+   }
+   cout << s << endl;
+}
+[[in]]--------------------------------------------------
+riuc
+387d+
+l ji+
+_-_-_-_-_
+shusss
+bartu
+escac
+rack
+wows
+period.
+.
+the end
+the end 2
+[[out]]-------------------------------------------------
+c++_sucks.
