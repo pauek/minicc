70230f69fd87832e399758f5eb39f3014e281fbd
Pau Fernández <pau.fernandez@upc.edu>
2015-08-14 14:55:15 +0200

    Cambio un test de nombre

--- test/parser/declstmt1.cc
+++ test/parser/declstmt1.cc
@@ -2,4 +2,4 @@ int main() {
    int a,
 }
 [[err]]-----------------------------------------------
-parser/semicolon1.cc[2:9-2:10]: Esperaba un nombre de variable aquí.
+parser/declstmt1.cc[2:9-2:10]: Esperaba un nombre de variable aquí.
