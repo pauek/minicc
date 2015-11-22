4786aa2341e0de5519d71cd9fc7fc32fa306b5af
Pau Fernández <pau.fernandez@upc.edu>
2015-11-22 22:44:24 +0100

    Añado un par más de tests del for

---
+++ test/parser/for3.cc
@@ -0,0 +1,5 @@
+int main() {
+   for (i;i;i) {
+      cout << i << endl;
+   }
+}
---
+++ test/parser/for4.cc
@@ -0,0 +1,5 @@
+int main() {
+   for (int i = 0, j = 1; i < 1 and j < 2; i++, j++) {
+      cout << i, j << endl;
+   }
+}
