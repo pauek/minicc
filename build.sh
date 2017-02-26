9fff2d9c69b05fa83904c6e74d344f22847c1ecc
Pau Fernández <pau.fernandez@upc.edu>
2017-02-26 14:54:10 +0100

    Cosas

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-clang++ -DDEBUG --std=c++11 -o minicc main.cc atom.cc lexer.cc file.cc
+clang++ -DDEBUG --std=c++11 -o minicc main.cc atom.cc lexer.cc file.cc debug.cc
+# clang++ -O3 --std=c++11 -o minicc main.cc atom.cc lexer.cc file.cc
