815048d8929bdfeb9a9cabc28df0131e41bba24b
Pau Fernández <pau.fernandez@upc.edu>
2017-02-27 21:34:00 +0100

    Progresssssss

--- ast.h
+++ ast.h
@@ -5,16 +5,54 @@ namespace ast {
 
 enum Tag {
    None = 0,
-#define AST(type, members) type,
+#define AST(T, members) tag##T,
 #include "ast.inc"
 #undef  AST
    TotalNodes,
 };
 
 enum OpType {
-   OP_ASSIGN,
-   OP_EQUALS,
-   OP_LESS
+   OP_BARBAR,
+   OP_AMPAMP,
+   OP_OR,
+   OP_AND,
+
+   OP_EQEQ,
+   OP_NOTEQ,
+   OP_LT,
+   OP_GT,
+   OP_LEQ,
+   OP_GEQ,
+
+   OP_NOT,
+   OP_AMP,
+   OP_BAR,
+   OP_XOR,
+   OP_PLUS,
+   OP_MINUS,
+   OP_STAR,
+   OP_SLASH,
+   OP_MOD,
+   OP_LSHIFT,
+   OP_RSHIFT,
+   OP_INC,
+   OP_DEC,
+   OP_POST_INC,
+   OP_POST_DEC,
+
+   OP_EQ,
+   OP_PLUSEQ,
+   OP_MINUSEQ,
+   OP_STAREQ,
+   OP_SLASHEQ,
+   OP_MODEQ,
+   OP_AMPEQ,
+   OP_BAREQ,
+   OP_XOREQ,
+   OP_LSHIFTEQ,
+   OP_RSHIFTEQ,
+
+   OP_ERROR,
 };
 
 // sizeof(Node) is the size of 'tag' & 'type' + padding to
@@ -25,63 +63,76 @@ struct Node {
    uint8_t data[];
 };
 
-#define   AST(type, members) struct t##type members;
+// declaration of structures
+#define   AST(T, members) struct type##T members;
 #include "ast.inc"
 #undef    AST
 
 #define AST_ACCESS(var, T, node) \
-   assert(node->tag == T); \
-   t##T *var = ((t##T *)node->data);
+   assert(node->tag == tag##T); \
+   type##T *var = ((type##T *)node->data);
 
+// _node functions dispatched for every type of structure
 #define AST(T, members) \
-   Node *_node(t##T data) { \
-      Node *n = (Node *)malloc(sizeof(Node) + sizeof(t##T)); \
+   Node *_node(type##T data) { \
+      Node *n = (Node *)malloc(sizeof(Node) + sizeof(type##T)); \
       assert(n); \
-      n->tag = T; \
-      *((t##T *)n->data) = data; \
+      n->tag = tag##T; \
+      *((type##T *)n->data) = data; \
       return n; \
    }
 #include "ast.inc"
 #undef  AST
 
-inline Node *_if_(Node *cond, Node *then, Node *els = 0) {
-   return _node((tIfStmt){ cond, then, els });
+// interface
+
+      void  print(Node *node);
+const char *op2str(OpType op);
+    OpType  str2op(const char *s_op);
+
+extern int indent_size;
+
+inline Node *If(Node *cond, Node *then, Node *els = 0) {
+   return _node((typeIfStmt){ cond, then, els });
 }
-inline Node *_while_(Node *cond, Node *block) {
-   return _node((tWhileStmt){ cond, block });
+inline Node *While(Node *cond, Node *block) {
+   return _node((typeWhileStmt){ cond, block });
 }
-inline Node *_for_(Node *bef, Node *cond, Node *aft, Node *block) {
-   return _node((tForStmt){ bef, cond, aft, block });
+inline Node *For(Node *bef, Node *cond, Node *aft, Node *block) {
+   return _node((typeForStmt){ bef, cond, aft, block });
 }
-inline Node *_binop_(Node *left, OpType op, Node *right) {
-   return _node((tBinOp){ op, left, right });
+inline Node *UnOp(const char *s_op, Node *operand) {
+   return _node((typeUnOp){ str2op(s_op), operand });
 }
-inline Node *_globalvar_(Atom *atom, Node *init = 0) {
-   return _node((tGlobalVar){ atom, init });
+inline Node *BinOp(Node *left, const char *s_op, Node *right) {
+   return _node((typeBinOp){ str2op(s_op), left, right });
 }
-inline Node *_localvar_(Atom *atom, Node *init = 0) {
-   return _node((tLocalVar){ atom, init });
+inline Node *GlobalVar(Atom *atom, Node *init = 0) {
+   return _node((typeGlobalVar){ atom, init });
 }
-inline Node *_int_(int i) {
-   return _node((tIntLiteral){ i });
+inline Node *LocalVar(Atom *atom, Node *init = 0) {
+   return _node((typeLocalVar){ atom, init });
 }
-inline Node *_float_(float f) {
-   return _node((tFloatLiteral){ f });
+inline Node *Literal(int i) {
+   return _node((typeIntLiteral){ i });
 }
-inline Node *_double_(double f) {
-   return _node((tDoubleLiteral){ f });
+inline Node *Literal(float f) {
+   return _node((typeFloatLiteral){ f });
 }
-inline Node *_label_(Atom *atom) {
-   return _node((tLabel){ atom });
+inline Node *Literal(double f) {
+   return _node((typeDoubleLiteral){ f });
 }
-inline Node *_block_(Array *stmts) {
-   return _node((tBlock){ stmts });
+inline Node *Label(Atom *atom) {
+   return _node((typeLabel){ atom });
 }
-inline Node *_block_() {
+inline Node *Block(Array *stmts) {
+   return _node((typeBlock){ stmts });
+}
+inline Node *Block() {
    Array *stmts = array::make(0, sizeof(Node *));
-   return _node((tBlock){ stmts });
+   return _node((typeBlock){ stmts });
 }
-Node *_block_(Node *first, ...) {
+Node *Block(Node *first, ...) {
    // @Speed: we should count arguments first, and create an array with that size??
    Array *stmts = array::make(0, sizeof(Node *));
    va_list args;
@@ -97,21 +148,22 @@ Node *_block_(Node *first, ...) {
       i++;
    }
    va_end(args);
-   return _node((tBlock){ stmts });
+   return _node((typeBlock){ stmts });
 }
-#define _block_BEGIN  _block_(
-#define _block_END    0)
+#define BlockBegin  Block(
+#define BlockEnd    0)
 
 inline void push(Node *node, Node *child) {
    AST_ACCESS(block, Block, node);
    array::push(block->nodes, &child);
 }
 
-
-      void  print(Node *node);
-const char *op2str(OpType op);
-
-extern int indent_size;
+inline Node *Equals(Node *left, Node *right) {
+   return BinOp(left, "==", right);
+}
+inline Node *Assign(Node *left, Node *right) {
+   return BinOp(left, "=", right);
+}
 
 }
 
@@ -125,13 +177,107 @@ namespace ast {
 
 const char *op2str(OpType op) {
    switch (op) {
-   case OP_ASSIGN: return "=";  break;
-   case OP_EQUALS: return "=="; break;
-   case OP_LESS:   return "<";  break;
+   case OP_BARBAR:     return "||"; break;
+   case OP_AMPAMP:     return "&&"; break;
+   case OP_OR:         return "or"; break;
+   case OP_AND:        return "and"; break;
+   case OP_EQEQ:       return "=="; break;
+   case OP_NOTEQ:      return "!="; break;
+   case OP_LT:         return "<"; break;
+   case OP_GT:         return ">"; break;
+   case OP_LEQ:        return "<="; break;
+   case OP_GEQ:        return ">="; break;
+   case OP_NOT:        return "!"; break;
+   case OP_AMP:        return "&"; break;
+   case OP_BAR:        return "|"; break;
+   case OP_XOR:        return "^"; break;
+   case OP_PLUS:       return "+"; break;
+   case OP_MINUS:      return "-"; break;
+   case OP_STAR:       return "*"; break;
+   case OP_SLASH:      return "/"; break;
+   case OP_MOD:        return "%"; break;
+   case OP_LSHIFT:     return "<<"; break;
+   case OP_RSHIFT:     return ">>"; break;
+   case OP_INC:        return "++"; break;
+   case OP_DEC:        return "--"; break;
+   case OP_POST_INC:   return "++"; break;
+   case OP_POST_DEC:   return "--"; break;
+   case OP_EQ:         return "="; break;
+   case OP_PLUSEQ:     return "+="; break;
+   case OP_MINUSEQ:    return "-="; break;
+   case OP_STAREQ:     return "*="; break;
+   case OP_SLASHEQ:    return "/="; break;
+   case OP_MODEQ:      return "%="; break;
+   case OP_AMPEQ:      return "&="; break;
+   case OP_BAREQ:      return "|="; break;
+   case OP_XOREQ:      return "^="; break;
+   case OP_LSHIFTEQ:   return "<<="; break;
+   case OP_RSHIFTEQ:   return ">>="; break;
    default:
       return "<?>";
    }
 }
+
+#define int2s(s)   (uint8_t(s[0])*256 + uint8_t(s[1]))
+#define int2(a, b) (uint8_t(a)*256    + uint8_t(b))
+
+OpType str2op(const char *s_op) {
+   switch (strlen(s_op)) {
+   case 1:
+      switch (s_op[0]) {
+      case '<': return OP_LT;
+      case '>': return OP_GT;
+      case '!': return OP_NOT;
+      case '&': return OP_AMP;
+      case '|': return OP_BAR;
+      case '^': return OP_XOR;
+      case '+': return OP_PLUS;
+      case '-': return OP_MINUS;
+      case '*': return OP_STAR;
+      case '/': return OP_SLASH;
+      case '%': return OP_MOD;
+      case '=': return OP_EQ;
+      }
+      break;
+   case 2:
+      switch (int2s(s_op)) {
+      case int2('|', '|'): return OP_BARBAR;
+      case int2('&', '&'): return OP_AMPAMP;
+      case int2('o', 'r'): return OP_OR;
+      case int2('=', '='): return OP_EQEQ;
+      case int2('!', '='): return OP_NOTEQ;
+      case int2('<', '='): return OP_LEQ;
+      case int2('>', '='): return OP_GEQ;
+      case int2('<', '<'): return OP_LSHIFT;
+      case int2('>', '>'): return OP_RSHIFT;
+      case int2('+', '+'): return OP_INC;
+      case int2('-', '-'): return OP_DEC;
+      case int2('+', '='): return OP_PLUSEQ;
+      case int2('-', '='): return OP_MINUSEQ;
+      case int2('*', '='): return OP_STAREQ;
+      case int2('/', '='): return OP_SLASHEQ;
+      case int2('%', '='): return OP_MODEQ;
+      case int2('&', '='): return OP_AMPEQ;
+      case int2('|', '='): return OP_BAREQ;
+      case int2('^', '='): return OP_XOREQ;
+      }
+      break;
+   case 3:
+      if (0 == strncmp(s_op, "and", 3)) {
+         return OP_AND;
+      } else if (0 == strncmp(s_op, "<<=", 3)) {
+         return OP_LSHIFTEQ;
+      } else if (0 == strncmp(s_op, ">>=", 3)) {
+         return OP_RSHIFTEQ;
+      } else if (0 == strncmp(s_op, "_++", 3)) {
+         return OP_POST_INC;
+      } else if (0 == strncmp(s_op, "_--", 3)) {
+         return OP_POST_DEC;
+      }
+   }
+   return OP_ERROR;
+}
+   
 
 struct PrintState {
    int level;
@@ -142,11 +288,11 @@ const int MAX_INDENT = 1024;
 int indent_size = 3;
 
 inline bool is_control(Node *node) {
-   return node->tag == IfStmt || node->tag == WhileStmt || node->tag == ForStmt;
+   return node->tag == tagIfStmt || node->tag == tagWhileStmt || node->tag == tagForStmt;
 }
 
 inline bool has_semicolon(Node *node) {
-   return !is_control(node) && node->tag != Block && node->tag != Label;
+   return !is_control(node) && node->tag != tagBlock && node->tag != tagLabel;
 }
 
 static char *_indent(int level) {
@@ -170,13 +316,13 @@ static void _print(PrintState* state, Buffer *B, Node *node) {
       return;
    }
 
-#define CASE(T) case T: { AST_ACCESS(it, T, node);
+#define CASE(T) case tag##T: { AST_ACCESS(it, T, node);
 #define END     break; }
 
    switch (node->tag) {
    CASE(IntLiteral)    printf(B, "%d", it->val); END
-   CASE(FloatLiteral)  printf(B, "%f", it->val); END
-   CASE(DoubleLiteral) printf(B, "%e", it->val); END
+   CASE(FloatLiteral)  printf(B, "%g", it->val); END
+   CASE(DoubleLiteral) printf(B, "%g", it->val); END
    CASE(Label)
       printf(B, "%s:", it->atom->str);
    END
@@ -227,9 +373,18 @@ static void _print(PrintState* state, Buffer *B, Node *node) {
       state->level--;
       printf(B, "%s}", _indent(state->level));
    END
+   CASE(UnOp)
+      if (it->op == OP_POST_INC || it->op == OP_POST_DEC) {
+         _print(state, B, it->operand);
+         printf(B, "%s", op2str(it->op));
+      } else {
+         printf(B, "%s", op2str(it->op));
+         _print(state, B, it->operand);
+      }
+   END
    CASE(BinOp)
       _print(state, B, it->left);
-      printf(B, " %s ", op2str(it->op));
+      printf(B, " %s ", op2str(it->op), it->op);
       _print(state, B, it->right);
    END
    default:
@@ -245,41 +400,6 @@ void print(Buffer *B, Node *node) {
    PrintState state = { 0 };
    _print(&state, B, node);
    printf(B, "\n"); // @Hack: do this well
-}
-
-
-void test() {
-   using atom::atom;
-
-   Node *a = _localvar_(atom("a"));
-   Node *b = _localvar_(atom("b"));
-   Node *i = _localvar_(atom("i"));
-
-   Node *B = 
-   _block_BEGIN
-      _label_(atom("blah")),
-      _int_(5),
-      _binop_(a, OP_ASSIGN, _binop_(b, OP_EQUALS, _float_(1.4f))),
-      _for_(_binop_(i, OP_ASSIGN, _int_(0)), 
-            _binop_(i, OP_LESS, _int_(10)), 
-            0, 
-         _block_BEGIN
-            _int_(3),
-         _block_END
-      ),
-      _if_(
-         _binop_(a, OP_EQUALS, _int_(7)),
-         _block_BEGIN
-            _binop_(a, OP_ASSIGN, _int_(10)),
-            _binop_(b, OP_ASSIGN, _int_(15)),
-         _block_END
-      ),
-   _block_END;
-
-   Buffer *buf = buf::make();
-   print(buf, B);
-   printf("[%d, %d] %s", buf->len, buf->avail, buf->str);
-   buf::free(buf);
 }
 
 }
--- main.cc
+++ main.cc
@@ -22,9 +22,59 @@ void init() {
 	atom::init();
 }
 
+using namespace ast;
+
+ast::Node *for_i(Node *var, int start, int end, int incr, Node *block) {
+   Node *nincr = 0;
+   if (incr == 1) {
+      nincr = UnOp("_++", var);
+   } else {
+      nincr = BinOp(var, "+=", Literal(incr));
+   }
+   return For(BinOp(var, "=", Literal(start)),
+              BinOp(var, "<", Literal(end)),
+              nincr,
+              block);
+}
+
+
+void ast_test() {
+   using atom::atom;
+   using namespace ast;
+
+   // @Incomplete: We are missing the types!!!!
+
+   Node *a = LocalVar(atom("a"));
+   Node *b = LocalVar(atom("b"));
+   Node *i = LocalVar(atom("i"));
+
+   Node *B = 
+   BlockBegin
+      Label(atom("blah")),
+      Literal(5),
+      Assign(a, Equals(b, Literal(1.4142f))),
+      for_i(i, 0, 20, 2,
+         BlockBegin
+            Literal(3.0),
+         BlockEnd
+      ),
+      If(Equals(a, Literal(7)),
+         BlockBegin
+            Assign(a, Literal(10)),
+            Assign(b, Literal(15)),
+         BlockEnd
+      ),
+   BlockEnd;
+
+   Buffer *buf = buf::make();
+   print(buf, B);
+   printf("[%d, %d] %s", buf->len, buf->avail, buf->str);
+   buf::free(buf);
+}
+
 int main() {
    atom::init();
-   ast::test();
+   ast_test();
    // atom::print_all();
 }
 
