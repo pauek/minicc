cddcf0ac9cecaecf73677c97d52a1007f02febed
Pau Fernández <pau.fernandez@upc.edu>
2017-02-27 14:28:44 +0100

    más tests del ast

--- ast.h
+++ ast.h
@@ -43,7 +43,7 @@ struct Node {
 #include "ast.inc"
 #undef  AST
 
-inline Node *_if_(Node *cond, Node *then, Node *els) {
+inline Node *_if_(Node *cond, Node *then, Node *els = 0) {
    return _node((tIfStmt){ cond, then, els });
 }
 inline Node *_while_(Node *cond, Node *block) {
@@ -55,6 +55,9 @@ inline Node *_for_(Node *bef, Node *cond, Node *aft, Node *block) {
 inline Node *_binop_(OpType op, Node *left, Node *right) {
    return _node((tBinOp){ op, left, right });
 }
+inline Node *_globalvar_(Atom *atom, Node *init = 0) {
+   return _node((tGlobalVar){ atom, init });
+}
 inline Node *_localvar_(Atom *atom, Node *init = 0) {
    return _node((tLocalVar){ atom, init });
 }
@@ -63,6 +66,9 @@ inline Node *_int_(int i) {
 }
 inline Node *_float_(float f) {
    return _node((tFloatLiteral){ f });
+}
+inline Node *_double_(double f) {
+   return _node((tDoubleLiteral){ f });
 }
 inline Node *_label_(Atom *atom) {
    return _node((tLabel){ atom });
@@ -103,6 +109,14 @@ const int MAX_INDENT = 1024;
 
 int indent_size = 3;
 
+inline bool is_control(Node *node) {
+   return node->tag == IfStmt || node->tag == WhileStmt || node->tag == ForStmt;
+}
+
+inline bool has_semicolon(Node *node) {
+   return !is_control(node) && node->tag != Block && node->tag != Label;
+}
+
 static char *_indent(int level) {
    static char indent_str[MAX_INDENT] = {};
    assert((level * indent_size) < MAX_INDENT);
@@ -132,10 +146,26 @@ static void _print(PrintState* state, Buffer *B, Node *node) {
    CASE(FloatLiteral)  printf(B, "%f", it->val); END
    CASE(DoubleLiteral) printf(B, "%e", it->val); END
    CASE(Label)
-      printf(B, "%s%s:\n", _indent(state->level-1), it->atom->str);
+      printf(B, "%s:\n", it->atom->str);
+   END
+   CASE(IfStmt)
+      printf(B, "if (");
+      _print(state, B, it->cond);
+      printf(B, ") ");
+      _print(state, B, it->then);
+      if (it->els) {
+         printf(B, "else ");
+         _print(state, B, it->els);
+      }
+   END
+   CASE(WhileStmt)
+      printf(B, "while (");
+      _print(state, B, it->cond);
+      printf(B, ") ");
+      _print(state, B, it->block);
    END
    CASE(ForStmt)
-      printf(B, "%sfor (", _indent(state->level));
+      printf(B, "for (");
       _print(state, B, it->before);
       printf(B, "; ");
       _print(state, B, it->cond);
@@ -147,22 +177,28 @@ static void _print(PrintState* state, Buffer *B, Node *node) {
    CASE(LocalVar)
       printf(B, "%s", it->atom->str);
    END
+   CASE(GlobalVar)
+      printf(B, "%s", it->atom->str);
+   END
    CASE(Block)
       printf(B, "{\n");
       state->level++;
       for (size_t i = 0; i < array::len(it->nodes); i++) {
-         _print(state, B, *(Node **)array::get(it->nodes, i));
+         printf(B, "%s", _indent(state->level));
+         Node *sub = *(Node **)array::get(it->nodes, i);
+         _print(state, B, sub);
+         if (has_semicolon(sub)) {
+            printf(B, ";");
+         }
+         printf(B, "\n");
       }
       state->level--;
-      printf(B, "%s}\n", _indent(state->level));
+      printf(B, "%s}", _indent(state->level));
    END
    CASE(BinOp)
-      // @Incorrect: this should be done for expression statements not for binops
-      printf(B, "%s", _indent(state->level));
       _print(state, B, it->left);
       printf(B, " %s ", op2str(it->op));
       _print(state, B, it->right);
-      printf(B, ";\n");
    END
    default:
       printf(B, "<unknown>");
@@ -176,6 +212,7 @@ static void _print(PrintState* state, Buffer *B, Node *node) {
 void print(Buffer *B, Node *node) {
    PrintState state = { 0 };
    _print(&state, B, node);
+   printf(B, "\n"); // @Hack: do this well
 }
 
 
@@ -183,26 +220,36 @@ void test() {
    using atom::atom;
 
    Array *stmts = array::make(0, sizeof(Node *));
-   Node *b = _block_(stmts);
+   Node *bl = _block_(stmts);
 
    Node *lab = _label_(atom("blah"));
    array::push(stmts, &lab);
    Node *i = _int_(5);
    array::push(stmts, &i);
 
-   Node *assign = _binop_( 
-      OP_ASSIGN, 
-      _localvar_(atom("a")),
-      _binop_(OP_EQUALS, _localvar_(atom("b")), _float_(1.4f))
-   );
+   Node *a = _localvar_(atom("a"));
+   Node *b = _localvar_(atom("b"));
+
+   Node *assign = _binop_(OP_ASSIGN, a, _binop_(OP_EQUALS, b, _float_(1.4f)));
    array::push(stmts, &assign);
 
    Array *stmts2 = array::make(0, sizeof(Node *)); 
    Node *f = _for_(0, 0, 0, _block_(stmts2));
    array::push(stmts, &f);
 
+   Array *stmts3 = array::make(0, sizeof(Node *));
+   Node *ii = _if_(
+      _binop_(OP_EQUALS, a, _int_(7)),
+      _block_(stmts3)
+   );
+
+   Node *assign2 = _binop_(OP_ASSIGN, a, _int_(10));
+   array::push(stmts3, &assign2);
+
+   array::push(stmts, &ii);
+
    Buffer *buf = buf::make();
-   print(buf, b);
+   print(buf, bl);
    printf("[%d, %d] %s", buf->len, buf->avail, buf->str);
    buf::free(buf);
 }
