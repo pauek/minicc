2c46c6bbc252f501777701bef1e39735c17c8849
Pau Fernández <pau.fernandez@upc.edu>
2017-10-19 22:18:37 +0200

    Puliendo

--- src/lexer.cc
+++ src/lexer.cc
@@ -112,8 +112,7 @@ bool Lexer::next() {
       return false;
    }
    if (_curr == -1) {
-      _pos.lin = 1;
-      _pos.col = 1;
+      _pos = {1, 1};
       _linepos.push_back(0);
    } else if (_curr < _text.size() && _text[_curr] == '\n') {
       _pos.lin++;
