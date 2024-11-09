d031fd4799ce7777fb21817f5b2bf6152aea63f0
Pau Fernández <pau.fernandez@upc.edu>
2024-11-10 00:42:29 +0100

    Quito err + ordeno tests por nombre

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-directories=$(find tests -mindepth 1 -maxdepth 1 -type d | sed 's|tests/||')
+directories=$(find tests -mindepth 1 -maxdepth 1 -type d | sed 's|tests/||' | sort)
 for dir in $directories; do
 	files=$(find tests/${dir} -name "*.cc")
 	./minicc test ${dir} ${files}
