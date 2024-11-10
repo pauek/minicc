307aeb18f939da9432cf3c840599b2d19ef8822e
Pau Fernández <pau.fernandez@upc.edu>
2024-11-10 11:21:56 +0100

    Mejoro la ejecución de test

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+ERRFILE=errors.log
+
+echo
 directories=$(find tests -mindepth 1 -maxdepth 1 -type d | sed 's|tests/||' | sort)
+rm -f $ERRFILE
 for dir in $directories; do
 	files=$(find tests/${dir} -name "*.cc")
-	./minicc test ${dir} ${files}
+	./minicc test ${dir} ${files} 2>> $ERRFILE
 done
+errsize=$(wc -c $ERRFILE | cut -d' ' -f1)
+if [ $errsize -ne 0 ]; then
+	echo
+	echo "Errors written to '$ERRFILE'"
+fi
