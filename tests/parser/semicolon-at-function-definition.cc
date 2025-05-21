fcf8cc72dc8228631fb99dba9b67f90d007c722e
Pau Fernández <pau.fernandez@upc.edu>
2025-05-21 21:15:07 +0200

    New test

---
+++ tests/parser/semicolon-at-function-definition.cc
@@ -0,0 +1,3 @@
+int f() {};
+[[err]]-----------------------------------------------
+tests/parser/semicolon-at-function-definition.cc[1:11-1:11]: No termines las definiciones de funciones con un punto y coma.
