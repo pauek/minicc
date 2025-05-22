1a7a380e40e9e8e87b08bfe9127c341bc7c021f5
Pau Fernández <pau.fernandez@upc.edu>
2025-05-22 16:30:51 +0200

    Cosmetics

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ directories=$(find tests -mindepth 1 -maxdepth 1 -type d | sed 's|tests/||' | so
 rm -f $ERRFILE
 for dir in $directories; do
 	files=$(find tests/${dir} -name "*.cc")
-	./minicc test ${dir} ${files} 2>> $ERRFILE
+	./minicc test ${dir} ${files} 2>> $ERRFILE 
 done
 errsize=$(wc -c $ERRFILE | cut -d' ' -f1)
 if [ $errsize -ne 0 ]; then
