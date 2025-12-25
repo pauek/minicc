933232ca61cf71a92fe9d724459262d9a91fb969
Pau Fernández <pau.fernandez@upc.edu>
2025-12-25 20:10:47 +0100

    Refactored main.zig

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,14 +1,15 @@
 const std = @import("std");
 const Lexer = @import("lexer.zig").Lexer;
+const Allocator = std.mem.Allocator;
 
 const MAX_BYTES: usize = 10 * 1024 * 1024; // 10MB
 
-pub fn read_file(allocator: std.mem.Allocator, path: []const u8) ![:0]u8 {
+pub fn read_file(allocator: Allocator, path: []const u8) ![:0]u8 {
     const content = try std.fs.cwd().readFileAllocOptions(allocator, path, MAX_BYTES, null, .of(u8), 0);
     return content; // The caller is responsible for freeing the content.
 }
 
-pub fn show_file_tokens(allocator: std.mem.Allocator, path: []const u8, writer: *std.io.Writer) !void {
+pub fn show_file_tokens(allocator: Allocator, path: []const u8, writer: *std.io.Writer) !void {
     const read_buffer = try read_file(allocator, path);
     defer allocator.free(read_buffer);
 
@@ -37,11 +38,12 @@ pub fn main() !void {
 
     if (args.len < 2) {
         try stdout.print("Usage: minicc <file>\n", .{});
-    } else {
-        for (args[1..]) |filename| {
-            try show_file_tokens(alloc, filename, stdout);
-        }
+        try stdout.flush();
+        return;
     }
 
+    for (args[1..]) |filename| {
+        try show_file_tokens(alloc, filename, stdout);
+    }
     try stdout.flush();
 }
